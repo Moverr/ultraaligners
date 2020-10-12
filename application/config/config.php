ddea4c61f6b40706f64be238da47b0536d90af79
Muyinda ROgers <moverr@gmail.com>
2020-10-12 17:17:29 +0300

    :Deuggigng

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'http://localhost:8084/ultraaligners/';
-$config['base_url'] = 'https://ultraaligners.com/sandbox/';
+$config['base_url'] = 'http://localhost:8084/ultraaligners/';
+// $config['base_url'] = 'https://ultraaligners.com/sandbox/';
 
 
 /*
