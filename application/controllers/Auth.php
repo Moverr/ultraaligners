a69f8af1d29961983419a6a205057b1156ddc013
Muyinda ROgers <moverr@gmail.com>
2020-10-12 22:52:26 +0300

    :Debuggging

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -26,13 +26,13 @@ class Auth extends CI_Controller
 
 		if (isset($_POST['username'])) {
 			$data = filter_forwarded_data($this);
-		
+
 			$username = $this->input->post('username');
 			$password = $this->input->post('password');
 
 			if ($username != "" && $password != "") {
 				//todo: send email to backend
- 
+
 				$curl = curl_init();
 
 				curl_setopt_array($curl, array(
@@ -43,11 +43,11 @@ class Auth extends CI_Controller
 					CURLOPT_TIMEOUT => 30,
 					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 					CURLOPT_CUSTOMREQUEST => "POST",
-					CURLOPT_POSTFIELDS => "{\"email\":\"".$username."\",\"password\":\"".$password."\",\"mode\":\"token\"}",
+					CURLOPT_POSTFIELDS => "{\"email\":\"" . $username . "\",\"password\":\"" . $password . "\",\"mode\":\"token\"}",
 					CURLOPT_HTTPHEADER => array(
 						"cache-control: no-cache",
 						"content-type: application/json"
-						
+
 					),
 				));
 
@@ -58,41 +58,47 @@ class Auth extends CI_Controller
 
 				if ($err) {
 					echo "cURL Error #:" . $err;
-					$this->load->view('auth/login',$err);
-					 
+					$this->load->view('auth/login', $err);
 				} else {
 					$resp_err = json_decode($response);
-					
-				
-					if(isset($resp_err->error->message)){
+
+
+					if (isset($resp_err->error->message)) {
 						$data['error'] =  $resp_err->error->message;
-						$this->load->view('auth/login',$data);
-					}else{
+						$this->load->view('auth/login', $data);
+					} else {
 						//todo: set sessions  ::
 						$data_res = json_decode($response);
-					  
-						$newdata = array( 							 
-							'first_name'  => $data_res->data->user->first_name, 
-							'last_name'  => $data_res->data->user->last_name, 
-							'email'  => $data_res->data->user->email, 
-							'token'     => $data_res->data->token, 
+
+						$newdata = array(
+							'first_name'  => $data_res->data->user->first_name,
+							'last_name'  => $data_res->data->user->last_name,
+							'email'  => $data_res->data->user->email,
+							'role'  => $data_res->data->user->role,
+							'token'     => $data_res->data->token,
 							'logged_in' => TRUE
-						 );  
-						 
-						 $this->session->set_userdata($newdata);
+						);
 
+						$this->session->set_userdata($newdata);
 
+						redirect(base_url() . "dashboard");
 
-						$this->load->view('auth/login');
+					 
 					}
-					
-					
 				}
 			}
-
-		
 		} else {
-			$this->load->view('auth/login');
+						 
+			$token = $this->session->userdata('token');
+			$email = $this->session->userdata('email');
+			$logged_in = $this->session->userdata('logged_in');
+			 if(isset($token) && isset($email) && isset($logged_in) && $logged_in == TRUE ){
+				redirect(base_url() . "dashboard");
+			 }else{
+				$this->load->view('auth/login');
+			 }
+
+			
 		}
 	}
 }
