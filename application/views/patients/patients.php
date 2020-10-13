672bdf83207d27e56370a5766945e67528ff94ae
Muyinda ROgers <moverr@gmail.com>
2020-10-13 04:59:58 +0300

    :Debuggging

--- application/views/patients/patients.php
+++ application/views/patients/patients.php
@@ -13,89 +13,40 @@
 							First Name
 						</th>
 						<th>
-							 Last Name
+							Last Name
 						</th>
 						<th>
-							Email Address 
+							Email Address
 						</th>
 					</tr>
 				</thead>
 
 				<tbody>
 
-				<?php 
+					<?php
+					foreach ($data as $record) {
+					?>
+						<tr>
+							<td>
+								1
+							</td>
+							<td>
+								<?=@$record->first_name; ?>
+							</td>
+							<td>
+							<?=@$record->last_name; ?>
+							</td>
+							<td>
+							<?=@$record->email; ?>
+							</td>
+						</tr>
+					<?php
 
-				?>
-					<tr>
-						<td>
-							1
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							01/04/2012
-						</td>
-						<td>
-							Default
-						</td>
-					</tr>
-					<tr class="table-active">
-						<td>
-							1
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							01/04/2012
-						</td>
-						<td>
-							Approved
-						</td>
-					</tr>
-					<tr class="table-success">
-						<td>
-							2
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							02/04/2012
-						</td>
-						<td>
-							Declined
-						</td>
-					</tr>
-					<tr class="table-warning">
-						<td>
-							3
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							03/04/2012
-						</td>
-						<td>
-							Pending
-						</td>
-					</tr>
-					<tr class="table-danger">
-						<td>
-							4
-						</td>
-						<td>
-							TB - Monthly
-						</td>
-						<td>
-							04/04/2012
-						</td>
-						<td>
-							Call in to confirm
-						</td>
-					</tr>
+					}
+
+					?>
+ 
+				 
 				</tbody>
 			</table>
 
