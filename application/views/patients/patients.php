a1624985abd2ab1864255245bd9c70f3481c8370
Muyinda ROgers <moverr@gmail.com>
2020-10-20 04:51:44 +0300

    :Debuggging

--- application/views/patients/patients.php
+++ application/views/patients/patients.php
@@ -2,7 +2,22 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
-			<a href="<?= base_url(); ?>patient" class="btn btn-primary">Add</a>
+
+			<form class="form-inline">
+				<a href="<?= base_url(); ?>patient" class="btn btn-primary">Add</a>
+
+				<div class="form-group">
+					<label for="exampleInputName2">Name</label>
+					<input type="text" class="form-control" id="exampleInputName2" placeholder="Jane Doe">
+				</div>
+				<div class="form-group">
+					<label for="exampleInputEmail2">Email</label>
+					<input type="email" class="form-control" id="exampleInputEmail2" placeholder="jane.doe@example.com">
+				</div>
+				<button type="submit" class="btn btn-default">Send invitation</button>
+			</form>
+
+
 			<table class="table">
 				<thead>
 					<tr>
@@ -29,11 +44,11 @@
 
 
 						foreach ($data as $record) {
-						 
+
 					?>
 							<tr>
 								<td>
-								<?= @$record->id; ?>
+									<?= @$record->id; ?>
 								</td>
 								<td>
 									<?= @$record->first_name; ?>
