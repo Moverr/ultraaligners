5e80ab1332553ed240f6c5366345196ebd99e9dd
Muyinda ROgers <moverr@gmail.com>
2020-10-24 15:59:10 +0300

    :Debuggging

--- application/views/progress/addprogress.php
+++ application/views/progress/addprogress.php
@@ -3,8 +3,8 @@
 	<div class="row">
 		<?php
 
-//	echo $patient;
-?>
+		//	echo $patient;
+		?>
 		<div class="col-md-12">
 			<?php
 			if (isset($error)) {
@@ -22,14 +22,18 @@
 					<label for="patient">
 						Patient
 					</label>
-					<select  required="true" class="form-control" id="patient" name="patient">
+					<select required="true" class="form-control" id="patient" name="patient">
 						<?php
 						foreach ($data as $record) {
 							if ($record->first_name != "") {
 
 
 						?>
-								<option <?php if(isset($patient)    ){ if($patient == $record->id) {echo  "selected=true"; } } ?>    value="<?= @$record->id; ?>"> <?= @$record->first_name . " " . $record->last_name; ?></option>
+								<option <?php if (isset($patient)) {
+											if ($patient == $record->id) {
+												echo  "selected=true";
+											}
+										} ?> value="<?= @$record->id; ?>"> <?= @$record->first_name . " " . $record->last_name; ?></option>
 						<?php
 							}
 						}
@@ -38,18 +42,18 @@
 				</div>
 
 
-			 
+
 
 				<div class="form-group">
 					<label for="details">
 						Details
 					</label>
-					<textarea type="text" class="form-control" id="details" name="details"><?=@$details;?></textarea>
+					<textarea type="text" class="form-control" id="details" name="details"><?= @$details; ?></textarea>
 				</div>
 
 
 
- 
+
 
 
 				<button type="submit" class="btn btn-primary">
