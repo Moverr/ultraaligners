938a34af7635ba5c395e85702a99cd2ba1e5c986
Muyinda ROgers <moverr@gmail.com>
2020-10-24 14:07:12 +0300

    :Debuggging

--- application/views/appointments/addappointment.php
+++ application/views/appointments/addappointment.php
@@ -2,13 +2,13 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
-		<?php
-				if (isset($error)) {
-				?>
-					<div class="alert alert-danger" role="alert"><?= $error; ?></div>
-				<?php
-				}
-				?>
+			<?php
+			if (isset($error)) {
+			?>
+				<div class="alert alert-danger" role="alert"><?= $error; ?></div>
+			<?php
+			}
+			?>
 
 
 			<form role="form" method="POST">
@@ -16,21 +16,21 @@
 
 				<div class="form-group">
 					<label for="patient">
-					Patient
+						Patient
 					</label>
-				 <select  class="form-control" id="patient" name="patient"  >
-				 <?php
-					foreach ($data as $record) {
-						if($record->first_name != ""){
+					<select class="form-control" id="patient" name="patient">
+						<?php
+						foreach ($data as $record) {
+							if ($record->first_name != "") {
 
-						 
-					?>
-						<option default="">	<?= @$record->first_name." ".$record->last_name; ?></option>
-						<?php 
-					}
-				}
-					?>
-				 </select>
+
+						?>
+								<option default=""> <?= @$record->first_name . " " . $record->last_name; ?></option>
+						<?php
+							}
+						}
+						?>
+					</select>
 				</div>
 
 
@@ -41,20 +41,20 @@
 					<input type="text" class="form-control" id="title" name="title" />
 				</div>
 
-				
+
 				<div class="form-group">
 					<label for="details">
 						Details
 					</label>
-					<textarea type="text" class="form-control" id="details" name="details" ></textarea>
+					<textarea type="text" class="form-control" id="details" name="details"></textarea>
 				</div>
 
 
 
 
- 
 
- 
+
+
 				<button type="submit" class="btn btn-primary">
 					Submit
 				</button>
