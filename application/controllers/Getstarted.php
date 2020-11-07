2e6331470910afacabf57cc2fcef713c708c1ec9
Muyinda ROgers <moverr@gmail.com>
2020-11-08 02:03:23 +0300

    Including Headers to the script

---
+++ application/controllers/Getstarted.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Getstarted extends CI_Controller
+{
+
+ 
+	public function index()
+	{
+		$this->load->view('get_started');
+			
+	 
+	}
+}
