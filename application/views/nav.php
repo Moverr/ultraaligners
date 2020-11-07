c7eabbd0af23ffa858b7f80df786a99cfeb3558d
Muyinda ROgers <moverr@gmail.com>
2020-11-08 02:05:30 +0300

    Get Started

---
+++ application/views/nav.php
@@ -0,0 +1,130 @@
+<nav class="navbar navbar-dropdown beta-menu align-items-center navbar-fixed-top navbar-toggleable-sm">
+
+			<div class="collapse menu-content-top">
+				<div class="container">
+					<div class="d-flex justify-content-between">
+						<div class="pre-header-left">
+							<div class="content-left-side" data-app-modern-menu="true">
+								<marquee direction="left"><a class="content-link link mbr-black text-primary display-4" href="https://ultraaligner.com/news.html">Welcome to Ultra Aligners! We offer Free advice and diagnosis. Our dentists will create your treatment plan.</a></marquee>
+							</div>
+						</div>
+						<div class="pre-header-right">
+							<div class="content-right-side" data-app-modern-menu="true">
+								<div class="icons-block">
+									<a class="content-link link mbr-black display-4" href="#"><span class="socicon socicon-facebook mbr-iconfont mbr-iconfont-btn" style="color: rgb(0, 127, 255);"></span>
+
+									</a>
+									<a class="content-link link mbr-black display-4" href="#"><span class="socicon socicon-twitter mbr-iconfont mbr-iconfont-btn" style="color: rgb(0, 127, 255);"></span>
+
+									</a>
+									<a class="content-link link mbr-black display-4" href="#"><span class="fa fa-instagram mbr-iconfont mbr-iconfont-btn" style="color: rgb(0, 127, 255);"></span>
+
+									</a> <a class="content-link link mbr-black text-primary display-4" href="https://wa.me/447795116069"><span class="socicon socicon-whatsapp mbr-iconfont mbr-iconfont-btn" style="color: rgb(0, 127, 255);"></span>
+
+									</a>
+
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="menu-content-middle">
+				<div class="container">
+					<div class="d-flex align-items-center">
+						<div class="menu-logo">
+							<div class="navbar-brand">
+								<span class="navbar-logo">
+									<a href="index.html#top">
+										<img src="assets/images/ultra-bg-removed-462x123.png" alt="" title="" style="height: 3.8rem;">
+									</a>
+								</span>
+								<span class="navbar-caption-wrap"><a href="index.html#top" class="brand-link mbr-black mbr-bold text-info display-7">ULTRA ALIGNERS</a></span>
+							</div>
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent, #topLine" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+								<div class="hamburger">
+									<span></span>
+									<span></span>
+									<span></span>
+									<span></span>
+								</div>
+							</button>
+						</div>
+						<div class="menu-content-right">
+							<div class="info-widget">
+								<a href="mailto:info@ultraaligners.com"><span class="widget-icon mbr-iconfont mdi-communication-email"></span></a>
+								<div class="widget-content display-4">
+									<p class="widget-title mbr-fonts-style mbr-bold display-7">Email</p>
+									<p class="widget-text mbr-fonts-style display-4"><a href="mailto:information@ultraaligners.com" class="text-black">information@ultraaligners.com</a></p>
+								</div>
+							</div>
+							<div class="info-widget">
+								<a href="tel:+447795116069"><span class="widget-icon mbr-iconfont mdi-action-perm-phone-msg"></span></a>
+								<div class="widget-content display-4">
+									<p class="widget-title mbr-fonts-style mbr-bold display-7">Call Us
+									</p>
+									<p class="widget-text mbr-fonts-style display-4"><a href="tel:+447795116069" class="text-black">(+44) 7795116069</a></p>
+								</div>
+							</div>
+							<div class="info-widget">
+								<span class="widget-icon mbr-iconfont fa-book fa"></span>
+								<div class="widget-content">
+									<p class="widget-title mbr-fonts-style mbr-bold display-7">Knowledge Base</p>
+									<p class="widget-text mbr-fonts-style display-4"><a href="https://dentistry.uic.edu/patients/tooth-alignment-adults-guide" target="_blank" class="text-secondary text-primary">View Resources</a></p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="menu-content-bottom order-1 order-lg-0 collapse navbar-collapse" id="navbarSupportedContent">
+				<div class="container">
+					<div class="menu-area-wrapper">
+						<div class="menu-area">
+							<div class="d-flex align-items-center justify-content-between">
+								<div class="navigation-top">
+									<div class="wrap">
+										<ul class="navbar-nav nav-dropdown js-float-line align-items-center" data-app-modern-menu="true">
+											<li class="nav-item">
+												<a class="nav-link link display-4" href="<?= base_url(); ?>"><span class="socicon socicon-homeadvisor mbr-iconfont mbr-iconfont-btn"></span>
+													Ultra Aligners
+												</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link link display-4" href="<?= base_url(); ?>#features3-2">
+												<span class="fa fa-bell-o mbr-iconfont mbr-iconfont-btn" style="font-size: 21px;"></span>Get Started</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link link display-4" href="<?= base_url(); ?>#tabs2-k"><span class="fa fa-medkit mbr-iconfont mbr-iconfont-btn" style="font-size: 21px;"></span>Treatable Cases</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link link display-4" href="<?= base_url(); ?>#timeline1-b"><span class="socicon socicon-shopify mbr-iconfont mbr-iconfont-btn" style="font-size: 21px;"></span>Producdts</a>
+											</li>
+											<li class="nav-item">
+												<a class="nav-link link display-4" href="<?= base_url(); ?>#extContacts3-9"><span class="mdi-action-question-answer mbr-iconfont mbr-iconfont-btn"></span>Enquiries</a>
+											</li>
+
+											<li class="nav-item">
+												<a class="nav-link link display-4" href="<?= base_url(); ?>auth"><span class="fa fa-user-circle-o mbr-iconfont mbr-iconfont-btn"></span>Login</a>
+											</li>
+
+										</ul>
+									</div>
+								</div>
+								<div class="right-box">
+									<div class="ml-auto">
+										<div class="navbar-buttons mbr-section-btn">
+											
+											<a class="btn btn-sm btn-info display-4" href="<?= base_url(); ?>getstarted">
+												Get Started
+											</a></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
