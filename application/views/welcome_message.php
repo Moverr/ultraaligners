28e5cdbd293c497a329c301c8755403b9d9c054a
Muyinda ROgers <moverr@gmail.com>
2020-11-07 03:31:58 +0300

    :Debuggigng

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -7,34 +7,61 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-	<link rel="shortcut icon" href="assets/images/logo-ultra-aligners-122x106.png" type="image/x-icon">
+ 
 	<meta name="description" content="">
 
 
 	<title>Ultra Ligners</title>
-	<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
-	<link rel="stylesheet" href="assets/simple-line-icons/simple-line-icons.css">
-	<link rel="stylesheet" href="assets/icons-mind/style.css">
-	<link rel="stylesheet" href="assets/web/assets/mobirise-icons2/mobirise2.css">
-	<link rel="stylesheet" href="assets/icon54-v4/style.css">
-	<link rel="stylesheet" href="assets/bootstrap-material-design-font/css/material.css">
-	<link rel="stylesheet" href="assets/icon54-v3/style.css">
-	<link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
-	<link rel="stylesheet" href="assets/icon54/style.css">
-	<link rel="stylesheet" href="assets/tether/tether.min.css">
-	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
-	<link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
-	<link rel="stylesheet" href="assets/dropdown/css/style.css">
-	<link rel="stylesheet" href="assets/socicon/css/styles.css">
-	<link rel="stylesheet" href="assets/animatecss/animate.min.css">
-	<link rel="stylesheet" href="assets/as-pie-progress/css/progress.min.css">
-	<link rel="stylesheet" href="assets/theme/css/style.css">
-	<link rel="stylesheet" href="assets/recaptcha.css">
-	<link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
-	<link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/font-awesome/css/font-awesome.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/simple-line-icons/simple-line-icons.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/icons-mind/style.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/web/assets/mobirise-icons2/mobirise2.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/icon54-v4/style.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/bootstrap-material-design-font/css/material.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/icon54-v3/style.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/web/assets/mobirise-icons/mobirise-icons.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/icon54/style.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/tether/tether.min.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/bootstrap/css/bootstrap-grid.min.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/bootstrap/css/bootstrap-reboot.min.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/dropdown/css/style.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/socicon/css/styles.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/animatecss/animate.min.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/as-pie-progress/css/progress.min.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/theme/css/style.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/recaptcha.css">
+	<link rel="preload" as="style" href="<?=base_url(); ?>assets/mobirise/css/mbr-additional.css">
+	<link rel="stylesheet" href="<?=base_url(); ?>assets/mobirise/css/mbr-additional.css" type="text/css">
 
 
+
+  <!-- Smartsupp Live Chat script -->
+  <script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '2aa16c9052ddc25c1b611b38139c89c4c301aa15';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+  
+<meta name="theme-color" content="#1034a6">
+<link rel="manifest" href="manifest.json">
+<script src="<?=base_url(); ?>sw-connect.js"></script>
+<meta name="apple-mobile-web-app-capable" content="yes">
+<link rel="apple-touch-startup-image" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-640x1136.png">
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-750x1334.png">
+<link rel="apple-touch-startup-image" media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="assets/images/apple-launch-1242x2208.png">
+<link rel="apple-touch-startup-image" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" href="assets/images/apple-launch-1125x2436.png">
+<link rel="apple-touch-startup-image" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-1536x2048.png">
+<link rel="apple-touch-startup-image" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-1668x2224.png">
+<link rel="apple-touch-startup-image" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" href="assets/images/apple-launch-2048x2732.png">
+<meta name="apple-mobile-web-app-status-bar-style" content="default">
+<meta name="apple-mobile-web-app-title" content="ULTRA ALIGNERS">
+<link rel="apple-touch-icon" href="apple-touch-icon.png">
 
 
 </head>
