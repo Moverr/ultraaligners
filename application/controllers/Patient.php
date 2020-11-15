7200e1d9d68929b827d389d8065069976715542c
Muyinda ROgers <moverr@gmail.com>
2020-11-15 03:54:19 +0300

    :Debugging

--- application/controllers/Patient.php
+++ application/controllers/Patient.php
@@ -29,7 +29,71 @@ class Patient extends CI_Controller
 
 
 		if (isset($_POST['firstname'])) {
-			echo " Complete Editing ";
+
+
+
+			$username = $this->input->post('email');
+			$password = $this->input->post('password');
+
+			$first_name = $this->input->post('firstname');
+			$last_name = $this->input->post('lastname');
+			$role = "5";
+			$random_string = chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90));
+
+
+
+
+			if ($_POST['firstname'] == "" ||  $_POST['lastname'] == "") {
+				//todo: make it succesful
+
+				$data['section'] = "patient_form";
+				$data['error'] = "Fill in the mandatory fields ";
+				$this->load->view('dashboard/dashboard', $data);
+			} else {
+
+				$curl = curl_init();
+
+				curl_setopt_array($curl, array(
+					CURLOPT_URL => "https://ultraaligners.com/public/ultraaligners/users",
+					CURLOPT_RETURNTRANSFER => true,
+					CURLOPT_ENCODING => "",
+					CURLOPT_MAXREDIRS => 10,
+					CURLOPT_TIMEOUT => 30,
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_CUSTOMREQUEST => "POST",
+					//	CURLOPT_POSTFIELDS => "{\"status\":\"active\",\"first_name\":\"".$first_name."\",\"last_name\":\"".$last_name."\",\"email\":\"".$username."\",\"role\":\"".$role."\",\"password\":\"".$password."\",\"token\":\"".$random_string."\"}",
+					CURLOPT_POSTFIELDS => "{\"status\":\"active\",\"first_name\":\"" . $_POST['firstname'] . "\",\"last_name\":\"" . $_POST['lastname'] . "\",\"email\":\"" . $username . "\",\"role\":\"" . $role . "\",\"password\":\"" . $password . "\",\n\"token\":\"" . $random_string . "\"\n\t\n\t\n}",
+
+
+					CURLOPT_HTTPHEADER => array(
+						"authorization:bearer " . $token,
+						"cache-control: no-cache",
+						"content-type: application/json"
+
+					),
+				));
+
+				$response = curl_exec($curl);
+				$err = curl_error($curl);
+				if ($err) {
+					echo "..............................";
+					var_dump($err);
+				} else {
+
+					$resp_err = json_decode($response);
+					if (isset($resp_err->error->message)) {
+						$data['error'] =  $resp_err->error->message;
+						$data['section'] = "patient_form";
+						$this->load->view('dashboard/dashboard', $data);
+					} else {
+						redirect(base_url() . "dashboard/patients");
+					}
+				}
+			}
+
+
+
+			// End
 		} else {
 
 
