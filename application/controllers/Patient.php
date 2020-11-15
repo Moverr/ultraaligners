b20706314ab40034a4af15b237b677fa34239e66
Muyinda ROgers <moverr@gmail.com>
2020-11-15 03:32:22 +0300

    token

--- application/controllers/Patient.php
+++ application/controllers/Patient.php
@@ -24,10 +24,10 @@ class Patient extends CI_Controller
 		$this->load->library('session');
 		$data = array();
 		$role = $this->session->userdata('role');
-
+		$token = $this->session->userdata('token');
 		{
 			$curl = curl_init();
-			
+
 			$url = "https://ultraaligners.com/public/ultraaligners/users?meta=total_count%2Cresult_count%2Cfilter_count&limit=10&offset=0&fields=role.*%2Cfirst_name.*%2Clast_name.*%2Cemail.*%2Cid&filter[id][contains]=1";
 
 			curl_setopt_array($curl, array(
