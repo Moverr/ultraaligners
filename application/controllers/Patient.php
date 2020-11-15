3a40f94d1fb7485ddda18dc688ac42d78369b060
Muyinda ROgers <moverr@gmail.com>
2020-11-15 03:05:33 +0300

    :Debuggging

--- application/controllers/Patient.php
+++ application/controllers/Patient.php
@@ -19,22 +19,25 @@ class Patient extends CI_Controller
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function edit($id){
-		$this->load->library('session'); 
+	public function edit($id)
+	{
+		$this->load->library('session');
 		$data = array();
 		$role = $this->session->userdata('role');
 
-			$data['section'] = "patient_form";
-			$data['role'] = $role;
-			$this->load->view('dashboard/dashboard', $data);
+		$data['section'] = "patient_form";
+		$data['role'] = $role;
+		$this->load->view('dashboard/dashboard', $data);
 	}
 
-	public function del($id){
+	public function del($id)
+	{
 		redirect(base_url() . "dashboard/patients");
 	}
 
-	public function search(){
-			//todo: get Patients in the system :: 
+	public function search()
+	{
+		//todo: get Patients in the system :: 
 		$quesry = 	$this->input->get('search', TRUE);
 		$this->load->library('session');
 		$token = $this->session->userdata('token');
@@ -45,7 +48,7 @@ class Patient extends CI_Controller
 		$curl = curl_init();
 
 		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://ultraaligners.com/public/ultraaligners/users?meta=total_count,result_count,filter_count&limit=200&offset=0&fields=*.*,role.*,first_name.*,last_name.*,email.*,id&filter[role][contains]=patients&q=".$quesry."",
+			CURLOPT_URL => "https://ultraaligners.com/public/ultraaligners/users?meta=total_count,result_count,filter_count&limit=200&offset=0&fields=*.*,role.*,first_name.*,last_name.*,email.*,id&filter[role][contains]=patients&q=" . $quesry . "",
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_ENCODING => "",
 			CURLOPT_MAXREDIRS => 10,
@@ -53,9 +56,9 @@ class Patient extends CI_Controller
 			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 			CURLOPT_CUSTOMREQUEST => "GET",
 			CURLOPT_HTTPHEADER => array(
-				"authorization:bearer ".$token, 
+				"authorization:bearer " . $token,
 				"cache-control: no-cache",
-				"content-type: application/json" 
+				"content-type: application/json"
 			),
 		));
 
@@ -75,47 +78,50 @@ class Patient extends CI_Controller
 			$data['section'] = "patients";
 			$data['meta'] = $responsedata->meta;
 			$data['data'] = $responsedata->data;
-			if(!isset($data['data'])){
+			if (!isset($data['data'])) {
 				redirect(base_url() . "auth");
-			}else{
+			} else {
 				$this->load->view('dashboard/dashboard', $data);
 			}
-
-			
 		}
 	}
 
 
 	public function index()
 	{
+		//todo:
+		$data = filter_forwarded_data($this);
+
 		$this->load->library('session');
+		$role = $this->session->userdata('role');
+		$data['role'] = $role;
+
 		$token = $this->session->userdata('token');
-		if(!isset($token)){
+		if (!isset($token)) {
 			redirect(base_url() . "auth");
 		}
 
 		if (isset($_POST['firstname'])) {
-			//todo:
-			$data = filter_forwarded_data($this);
+
 
 			$username = $this->input->post('email');
 			$password = $this->input->post('password');
 
 			$first_name = $this->input->post('firstname');
-			$last_name = $this->input->post('lastname');		 
+			$last_name = $this->input->post('lastname');
 			$role = "5";
-			  $random_string = chr(rand(65,90)) . chr(rand(65,90)) . chr(rand(65,90)) . chr(rand(65,90)) . chr(rand(65,90));
-			 
+			$random_string = chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90)) . chr(rand(65, 90));
 
-			 
 
-			if( $_POST['firstname'] == "" ||  $_POST['lastname'] == ""  ){
+
+
+			if ($_POST['firstname'] == "" ||  $_POST['lastname'] == "") {
 				//todo: make it succesful
-				$data = array();
+			
 				$data['section'] = "patient_form";
 				$data['error'] = "Fill in the mandatory fields ";
 				$this->load->view('dashboard/dashboard', $data);
-			}else{
+			} else {
 
 				$curl = curl_init();
 
@@ -127,12 +133,12 @@ class Patient extends CI_Controller
 					CURLOPT_TIMEOUT => 30,
 					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
 					CURLOPT_CUSTOMREQUEST => "POST",
-				//	CURLOPT_POSTFIELDS => "{\"status\":\"active\",\"first_name\":\"".$first_name."\",\"last_name\":\"".$last_name."\",\"email\":\"".$username."\",\"role\":\"".$role."\",\"password\":\"".$password."\",\"token\":\"".$random_string."\"}",
-					CURLOPT_POSTFIELDS => "{\"status\":\"active\",\"first_name\":\"".$_POST['firstname']."\",\"last_name\":\"".$_POST['lastname']."\",\"email\":\"".$username."\",\"role\":\"".$role."\",\"password\":\"".$password."\",\n\"token\":\"".$random_string."\"\n\t\n\t\n}",
+					//	CURLOPT_POSTFIELDS => "{\"status\":\"active\",\"first_name\":\"".$first_name."\",\"last_name\":\"".$last_name."\",\"email\":\"".$username."\",\"role\":\"".$role."\",\"password\":\"".$password."\",\"token\":\"".$random_string."\"}",
+					CURLOPT_POSTFIELDS => "{\"status\":\"active\",\"first_name\":\"" . $_POST['firstname'] . "\",\"last_name\":\"" . $_POST['lastname'] . "\",\"email\":\"" . $username . "\",\"role\":\"" . $role . "\",\"password\":\"" . $password . "\",\n\"token\":\"" . $random_string . "\"\n\t\n\t\n}",
 
-				
+
 					CURLOPT_HTTPHEADER => array(
-						"authorization:bearer ".$token,
+						"authorization:bearer " . $token,
 						"cache-control: no-cache",
 						"content-type: application/json"
 
@@ -141,29 +147,23 @@ class Patient extends CI_Controller
 
 				$response = curl_exec($curl);
 				$err = curl_error($curl);
-				if($err){
+				if ($err) {
 					echo "..............................";
 					var_dump($err);
-				}
-				else{
-				
+				} else {
+
 					$resp_err = json_decode($response);
 					if (isset($resp_err->error->message)) {
 						$data['error'] =  $resp_err->error->message;
 						$data['section'] = "patient_form";
 						$this->load->view('dashboard/dashboard', $data);
-					} else{
+					} else {
 						redirect(base_url() . "dashboard/patients");
 					}
-						
 				}
-				
 			}
-
-			 
-
 		} else {
-			$data = array();
+			 
 			$data['section'] = "patient_form";
 			$data['error'] = "Fill Mandatories";
 			$this->load->view('dashboard/dashboard', $data);
